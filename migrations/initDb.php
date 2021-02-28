922d11e1a4cf672d92b97695bb73795507e90f0e
coordingstone <9132651+coordingstone@users.noreply.github.com>
2021-02-28 21:03:48 +0100

    Updated init db to add testdata

--- migrations/initDb.php
+++ migrations/initDb.php
@@ -6,8 +6,13 @@ try {
     /** @var \Dashboard\Database\Db $db */
     $db = $container->get('Dashboard\Database\Db');
     $sql = file_get_contents('sql/init.sql');
-    $result = $db->getConnection()->exec($sql);
+    $testDataSql = file_get_contents('sql/test/testdata.sql');
+    $db->getConnection()->exec($sql);
     echo "Created db tables" . PHP_EOL;
+    $db->getConnection()->exec($testDataSql);
+    echo "Inserted testdata" . PHP_EOL;
+    echo "All done!" . PHP_EOL;
+
 } catch (Exception $exception) {
     echo "Something went wrong while creating database {$exception->getMessage()}" . PHP_EOL;
 }
